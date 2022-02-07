c7500f418634609bfccea86f174849791091c4de
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2022-02-07 19:02:51 +0300

    feat: initialize two sum task tests and task solution method

---
+++ Algorithms.Tasks/TwoSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Tasks
+{
+    public static class TwoSum
+    {
+        public static int[] GetIndicesOfTwoNumbers(this int[] nums, int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Algorithms.Tests/TwoSumTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Tasks;
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class TwoSumTests
+    {
+        [Test]
+        public void TwoSumTests_FirstTest()
+        {
+            // Arrange
+            var nums = new int[] {2, 7, 11, 15};
+            var target = 9;
+            var expected = new int[] {0, 1};
+
+            // Act
+            var actual = nums.GetIndicesOfTwoNumbers(target);
+
+            // Assert
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+    }
+}
