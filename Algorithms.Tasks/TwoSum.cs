c9091071e31edbf90d6b14b9a839d2a47beddc24
Vadzim Kurdzesau <KurdesoWadim@gmail.com>
2022-03-16 11:55:04 +0100

    refactor: use C# 6 namespace style

--- Algorithms.Tasks/TwoSum.cs
+++ Algorithms.Tasks/TwoSum.cs
@@ -1,34 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Algorithms.Tasks;
 
-namespace Algorithms.Tasks
+public static class TwoSum
 {
-    public static class TwoSum
+    /// <summary>
+    /// Gets indices of two elements in <paramref name="nums"/> which sum equals <paramref name="target"/>.
+    /// </summary>
+    /// <param name="nums">Array of elements whose indices get.</param>
+    /// <param name="target">Value that the sum of the elements in indices must equal.</param>
+    /// <returns><see cref="ValueTuple"/> with both elements indices.</returns>
+    /// <exception cref="ArgumentException">Thrown, if solution does not exist.</exception>
+    public static (int firstIndex, int secondIndex) GetIndicesOfTwoNumbers(this int[] nums, int target)
     {
-        /// <summary>
-        /// Gets indices of two elements in <paramref name="nums"/> which sum equals <paramref name="target"/>.
-        /// </summary>
-        /// <param name="nums">Array of elements whose indices get.</param>
-        /// <param name="target">Value that the sum of the elements in indices must equal.</param>
-        /// <returns><see cref="ValueTuple"/> with both elements indices.</returns>
-        /// <exception cref="ArgumentException">Thrown, if solution does not exist.</exception>
-        public static (int firstIndex, int secondIndex) GetIndicesOfTwoNumbers(this int[] nums, int target)
+        for (int i = 0; i < nums.Length; i++)
         {
-            for (int i = 0; i < nums.Length; i++)
+            for (int j = i + 1; j < nums.Length; j++)
             {
-                for (int j = i + 1; j < nums.Length; j++)
+                if (nums[j] + nums[i] == target)
                 {
-                    if (nums[j] + nums[i] == target)
-                    {
-                        return (i, j);
-                    }
+                    return (i, j);
                 }
             }
-
-            throw new ArgumentException($"There are no such elements in {nameof(nums)} which sum equals {target}.");
         }
+
+        throw new ArgumentException($"There are no such elements in {nameof(nums)} which sum equals {target}.");
     }
 }
