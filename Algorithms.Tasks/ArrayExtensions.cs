6acf2484de9cc992c7f56e720fef421f3da36bfb
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2022-07-06 11:31:12 +0200

    feat: complete remove duplicates task

---
+++ Algorithms.Tasks/ArrayExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Algorithms.Tasks
+{
+    /// <summary>
+    /// Contains array extension methods.
+    /// </summary>
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Removes the duplicates in-place such that each unique element appears only once keeping the same relative order.
+        /// </summary>
+        /// <param name="nums">An integer array of numbers sorted in non-decreasing order.</param>
+        /// <returns>The number of unique elements placed in the first <paramref name="nums"/> slots.</returns>
+        public static int RemoveDuplicates(this int[] nums)
+        {
+            if (nums.Length == 1)
+                return 1;
+
+            int currentNum = nums[0], k = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (currentNum != nums[i])
+                {
+                    nums[++k] = nums[i];
+                    currentNum = nums[i];
+                }
+            }
+
+            return ++k;
+        }
+    }
+}
---
+++ Algorithms.Tests/RemoveDuplicatesTests.cs
@@ -0,0 +1,36 @@
+﻿using Algorithms.Tasks;
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class RemoveDuplicatesTests
+    {
+        public static object[] TestCases => new object[]
+        {
+            new object[] { new int[] { 1, 1, 2 }, new int[] { 1, 2 }, 2 },
+            new object[] { new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, new int[] { 0, 1, 2, 3, 4 }, 5 },
+            new object[] { new int[] { 1 }, new int[] { 1 }, 1 },
+        };
+
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void RemoveDuplicates_RemovesDuplicatesAndReturnsExpectedK(int[] nums, int[] expectedArray, int expectedK)
+        {
+            // Act
+            var actualK = nums.RemoveDuplicates();
+
+            // Assert
+            Assert.AreEqual(expectedK, actualK);
+            AssertAreEqual(expectedArray, nums, actualK);
+        }
+
+        private static void AssertAreEqual(int[] expected, int[] actual, int k)
+        {
+            for (int i = 0; i < k; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
